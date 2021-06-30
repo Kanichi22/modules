c98dbb77209d2d65aafb5b43fbea3a327da7fe71
Kanichi <69150683+Kanichi22@users.noreply.github.com>
2021-06-30 16:47:57 +0700

    Create out.js

---
+++ out.js
@@ -0,0 +1,18 @@
+module.exports.config = {
+	name: "out",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "Kanichi",
+	description: "Rời nhóm",
+	commandCategory: "Admin",
+	usages: "[id]",
+	cooldowns: 3
+};
+
+module.exports.run = async function({ api, event, args }) {
+	 return api.getThreadInfo(event.threadID, (err, args) => {
+        if(err) throw err;
+        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+	    if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+	})
+}
