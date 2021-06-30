dac6a12cf972515a7edbbe490262277b37f6132a
Kanichi <69150683+Kanichi22@users.noreply.github.com>
2021-06-30 11:10:30 +0700

    Update

--- hi.js
+++ hi.js
@@ -8,9 +8,17 @@ module.exports.config = {
     usages: "",
     cooldowns: 0,
     denpendencies: {
-        "fs": ""
+        "fs": "",
+        "request": ""
     }
 };
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/noprefix/`;
+    if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "hi.gif")) request("https://media.discordapp.net/attachments/849164098024374283/859645612097798184/received_373965544066156.gif").pipe(fs.createWriteStream(dirMaterial + "hi.gif"));
+}
 module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
     const fs = require("fs");
     let dt = await api.getUserInfo(event.senderID);
@@ -21,6 +29,7 @@ module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils,
 			}
     if (event.body.toLowerCase() == "hi"){
         return api.sendMessage(msg,event.threadID,event.messageID);}
+        };
 module.exports.run = async ({ event, api, Currencies, args, utils }) => {
 return api.sendMessage("Dùng sai cách rồi lêu lêu",event.threadID)
 	}
